3da6a4b0122e3d550ac46d3e7693d12bd51748ee
chris <chris@Excalibur>
2011-03-08 21:53:59 -0600

    send and receive file

--- client/Client.py
+++ client/Client.py
@@ -3,11 +3,20 @@ from socket import *
 
 serverHost = '127.0.0.1'          # servername is localhost
 serverPort = 2000                 # use arbitrary port > 1024
+fileName = "README"               # read in file
+
+#send file to server
+def sendFile():
+    f = open(fileName,"rb")
+    for line in f:
+        s.send(line)
+    f.close()
 
 s = socket( AF_INET, SOCK_STREAM )  # create a TCP socket
 
 s.connect((serverHost, serverPort)) # connect to server on the port
-s.send( 'Hello world' )               # send the data
+sendFile()                          # send the data
 data = s.recv(1024)                 # receive up to 1K bytes
 
 print data
+
