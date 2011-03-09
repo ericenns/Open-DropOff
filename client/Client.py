74e92ef2a27272a9f4c2cb3283114c1f5e07a33e
Andrew Matsusaka <and.matsusaka@gmail.com>
2011-03-08 22:16:37 -0600

    Added options to execution of client.

--- client/Client.py
+++ client/Client.py
@@ -1,13 +1,27 @@
 import sys
+import getopt
 from socket import *
 
-serverHost = '127.0.0.1'          # servername is localhost
-serverPort = 2000                 # use arbitrary port > 1024
+optlist, args = getopt.getopt(sys.argv[1:], 's:p:o', ['server', 'port'] )
 
+for opt, val in optlist:
+    if opt == "-o" or opt == "--options":
+        print """Options are:
+            -s (--server): specify name of server to connect to
+            -p (--port): specify port number to connect to
+            """
+    elif opt == "-s" or opt == "--server":
+        serverHost = val
+    elif opt == "-p" or opt == "--port":
+        serverPort = val
+            
 s = socket( AF_INET, SOCK_STREAM )  # create a TCP socket
 
-s.connect((serverHost, serverPort)) # connect to server on the port
-s.send( 'Hello world' )               # send the data
-data = s.recv(1024)                 # receive up to 1K bytes
-
-print data
+try:
+    s.connect((serverHost, serverPort)) # connect to server on the port
+    s.send( 'Hey Travis!' )               # send the data
+    data = s.recv(1024)                 # receive up to 1K bytes
+    
+    print data
+except:
+    print "Unable to connect to server specified."
