f5358f9d376f2f4830c1b8ef055e48d35a26b3d5
Eric Enns <eric.enns@gmail.com>
2011-03-15 12:04:08 -0500

    Changed to getint method for retrieving the port

--- Server/server.py
+++ Server/server.py
@@ -84,7 +84,7 @@ class ODOTCPHandler(SocketServer.BaseRequestHandler):
 if __name__ == "__main__":
     #HOST, PORT = "localhost", 30000
     HOST = config.get("Network", "interface")
-    PORT = config.get("Network", int, "port")
+    PORT = config.getint("Network", "port")
 
     # Create the server, binding to localhost on port 9999
     server = SocketServer.TCPServer((HOST, PORT), ODOTCPHandler)
