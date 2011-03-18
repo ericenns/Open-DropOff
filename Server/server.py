ea9a6cabfd7f8b1300130dddc64022cd5b56dcec
Eric Enns <eric.enns@gmail.com>
2011-03-17 20:02:01 -0500

    Moved push into its own function

--- Server/server.py
+++ Server/server.py
@@ -52,46 +52,50 @@ class ODOTCPHandler(SocketServer.BaseRequestHandler):
         self.data = self.request.recv(80)
         command, arguments = self.data.split("\r\n", 1)
         if(command == "PUSH"):
-            # get the filename and filesize then tell the client to continue
-            filename, filesize = arguments.split("\r\n", 1)
-            filesize = int(filesize)
-            print "filesize: ", filesize
-            #echoing data for now, may be useful
-            self.request.send(self.data)
+            push(arguments)
             
-            #authenticate user information
-            self.data = self.request.recv(80)
-            userN, password = self.data.split("\r\n", 1)
+    def push(self, arguments):
+        # get the filename and filesize then tell the client to continue
+        filename, filesize = arguments.split("\r\n", 1)
+        filesize = int(filesize)
+        print "filesize: ", filesize
+        #echoing data for now, may be useful
+        self.request.send(self.data)
+        
+        #authenticate user information
+        self.data = self.request.recv(80)
+        userN, password = self.data.split("\r\n", 1)
+        
+        #verify user
+        
+        #verity password
+        
+        #write the files to a test sub-directory prevents 
+        #clogging up the server folder with random test files
+        #newfile = open("./testfiles/" + filename, "wb")
+        newfile = open(filename, "wb")
+        
+        #receives 100 bytes of the file at a time, loops until
+        #the whole file is received
+        #content = self.request.recv(filesize)
+        totalReceived = -1
+        
+        while totalReceived <= filesize:
+            if( totalReceived == -1 ):
+                totalReceived =  0
+            #print "looping!"
             
-            #verify user
-            
-            #verity password
-            
-            #write the files to a test sub-directory prevents 
-            #clogging up the server folder with random test files
-            #newfile = open("./testfiles/" + filename, "wb")
-            newfile = open(filename, "wb")
-            
-            #receives 100 bytes of the file at a time, loops until
-            #the whole file is received
-            #content = self.request.recv(filesize)
-            totalReceived = -1
-            
-            while totalReceived <= filesize:
-                if( totalReceived == -1 ):
-                    totalReceived =  0
-                #print "looping!"
-                
-                content = self.request.recv(RECEIVESIZE)
-                totalReceived += RECEIVESIZE
-                newfile.write(content)
+            content = self.request.recv(RECEIVESIZE)
+            totalReceived += RECEIVESIZE
+            newfile.write(content)
 
-            newfile.close() #close the file
-            
-            #send a response to the client
-            self.request.send("Received %s" % filename)
-            self.request.close()
-            print "Finished!\n"
+        newfile.close() #close the file
+        
+        #send a response to the client
+        self.request.send("Received %s" % filename)
+        self.request.close()
+        print "Finished!\n"
+        
             
 if __name__ == "__main__":
     #HOST, PORT = "localhost", 30000
