5173ea1b0fa3dfec93cc1bf8eb54db369da23f1f
Andrew Matsusaka <and.matsusaka@gmail.com>
2011-03-19 14:44:08 -0500

    Moved client.py into Client directory and refactored the code within it.  Also added the ability to create a new user from client.

--- Client/Controllers/RequestController.py
+++ Client/Controllers/RequestController.py
@@ -4,6 +4,7 @@ Created on Mar 17, 2011
 @author: Andrew
 '''
 
+import os
 import sys
 import socket
 sys.path.append('../../Server')
@@ -17,32 +18,82 @@ class RequestController(object):
     classdocs
     '''
     
-    def __init__(self):
-        '''
-        Constructor
-        '''
-        
-    def createConect(self, server, port):
-        self.sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )  # create a TCP socket
+    def __init__(self, server, port):
+        self.server = server
+        self.port = port
 
+    def connect(self):
+        self.sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )  # create a TCP socket
+        
         try:
-            self.sock.connect((server, port)) # connect to server on the port
+            self.sock.connect((self.server, self.port)) # connect to server on the port
         except:
-            print "Unable to connect to server specified. %s" % server
-               
+            print "Unable to connect to server specified. %s" % self.server
+    
+    def disconnect(self):
+        self.sock.close()
+        
+    #Creates a new user with the given information:
+    #params:    username    name to be used for new user
+    #            password    password for the new user
+    #returns: true if user creation was successful, false if not
+    def newUser(self, username, password):
+        self.connect()
+        
+        self.sock.send("NUSER\r\n%s\r\n%s" % username, password)
+        
+        response = self.sock.recv(RECEIVESIZE)
+        
+        if( response == "STAT 100"):
+            print "New user made!"
+            return True
+        else:
+            print "Unable to create user"
+            return False
+        
+        self.disconnect()
+
+    def login(self):
+        self.connect()
+        
+        #Simply data access for initial use
+        username = raw_input("Please enter your username: ")
+        print username
+        ##################Can be removed^^^
+        
+        self.sock.send("USER\r\n%s" % username)
+        response = self.sock.recv(RECEIVESIZE)
+        if(response == "OKAY"):
+            password = raw_input("Please enter your password: ")
+            self.sock.send("PASS\r\n%s" % password)
+            data = self.sock.recv(RECEIVESIZE)
+            response, key = data.split("\r\n",1)
+            if(response == "OKAY"):
+                self.key = key
+            else:
+                return "";
+        else:
+            return "";
+        
+        self.disconnect()
         
     #send file to server
     #params:    sock    connection to have file sent through
     #            filename    name of file to be sent
     #            filesize    size of file being sent
-    def sendFile(self, filename, filesize):
-        f = open(filename,"rb")
-        self.sock.send("PUSH\r\n%s\r\n%d" % (filename, filesize))
-        # wait for a response then start sending the file
-        reply = self.sock.recv(80)
-        print reply
+    def push(self, filename, filesize):
+        self.connect()
         
-        self.sock.send("%s\r\n%s" % ("JohnDoe","homie4life"))
+        f = open(filename,"rb")
+        self.sock.send("PUSH\r\n%s\r\n%i\r\n%s" % (filename, filesize, self.key))
+        # wait for a response then start sending the file
+        reply = self.sock.recv(RECEIVESIZE)
+        if(reply == "OKAY"):
+            print reply
+        else:
+            return
+        
+        #sock.send("%s\r\n%s" % ("JohnDoe","homie4life"))
         
         line = f.read(SENDSIZE)
         while line:
@@ -51,25 +102,17 @@ class RequestController(object):
                 sent += self.sock.send(line[sent:])
             line = f.read(SENDSIZE)
         f.close()
-            
-    #Send a user to be logged in to the connection
-    #params:    sock    connection to send username to
-    #            user    username to be sent over connection
-    def user(self, username):
-        self.sock.send("USER %s" % username)
-        #Could encrypt this as well
         
-    #Sends a password to the socket specified
-    #params:    sock    connection to send password to
-    #            password    password to be sent
-    def pwd(self, password):
-        self.sock.send("PASS %s" % password)
-        #Should add encryption at this point
+        data = self.sock.recv(80)
+        print data
+        
+        self.disconnect()
+
         
     #Sends a listing of contents request to connection
     #params:    sock    connection to send list request to
     #Returns: list of items returned by the server
-    def list(self):
+    def list(self, key):
         self.sock.send("LIST")
         #should figure out what format contents list should have
         contentsList = self.sock.recv(RECEIVESIZE)
@@ -77,28 +120,27 @@ class RequestController(object):
         return contentsList
         
     #Send a request for a file to be pulled from connection
-    #params:    sock    connection to have file pulled from
+    #params:    key    key that confirms identity of request sender
     #            filename    name of file to be pulled and saved
     #Returns: File data
     def pull(self, filename):
-        self.sock.send("PULL\r\n%s" % filename)
-        newfile = open(filename, "wb")
-        
-        #Receive a message indicating size of file coming
-        #filesize = self.sock.recv(whatever)
-        
-        #receives 100 bytes of the file at a time, loops until
-        #the whole file is received
-        #content = self.request.recv(filesize)
-        totalReceived = -1
-        
-        '''while totalReceived <= filesize:
-            if( totalReceived == -1 ):
-                totalReceived =  0
-                #print "looping!"
+        self.sock.send("PULL\r\n%s\r\n%s" % (filename, self.key))
+        arguments = self.sock.recv(80)
+        command, filesize = arguments.split("\r\n", 1)
+        filesize = int(filesize)
+        if command == "RECV":
+            self.sock.send("SEND")
+            newfile = open(filename, "wb")
+            totalReceived = -1
+            
+            while totalReceived <= filesize:
+                if( totalReceived == -1 ):
+                    totalReceived =  0
                 
                 content = self.sock.recv(RECEIVESIZE)
                 totalReceived += RECEIVESIZE
                 newfile.write(content)
-        '''
-        newfile.close()
+            
+            newfile.close() #close the file
+        else:
+            print "FAILURE!"
