8e3a934ec41573d22231f66f55f85f6e6a3a8bbc
mobileProgrammer <Mike@140.193.239.101>
2011-03-20 14:06:17 -0600

    Database changes patch 2 - should answer most if not all of our needs to this project.

--- Server/install.sql
+++ Server/install.sql
@@ -1,36 +1,60 @@
------------------------------------------
+-- =======================================
 -- Fresh install
------------------------------------------
+-- =======================================
 -- Create users table
 CREATE TABLE users (
 	username VARCHAR(255) PRIMARY KEY, 
 	password_hash CHAR(64) NOT NULL, 
-	quota BIGINT UNSIGNED) ENGINE = INNODB;
+	quota BIGINT UNSIGNED,
+	salt CHAR(32) NOT NULL) ENGINE = INNODB;
 
 -- Create files table
 CREATE TABLE files (
 	file_id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-	filename VARCHAR(255) NOT NULL, 
-	path VARCHAR(4096) NOT NULL, 
+	client_path VARCHAR(4096) NOT NULL, 
+	server_path VARCHAR(4096) NOT NULL, 
+	version SMALLINT UNSIGNED,
 	last_modified TIMESTAMP, 
 	last_author VARCHAR(255), 
-	version SMALLINT UNSIGNED,
-	directory BOOL, -- is the file a directory?
+	directory BOOL DEFAULT 0, -- is the file a directory?
 	size BIGINT UNSIGNED,
-	parent BIGINT UNSIGNED,
-	FOREIGN KEY (parent) REFERENCES files(file_id)) ENGINE = INNODB;
+	checksum CHAR(32) NOT NULL,
+	deleted BOOL DEFAULT 0
+	) ENGINE = INNODB;
+
+-- file history and version control
+CREATE TABLE file_history (
+	file_id BIGINT UNSIGNED, 
+	version SMALLINT UNSIGNED,
+	client_path VARCHAR(4096) NOT NULL, 
+	server_path VARCHAR(4096) NOT NULL, 
+	last_modified TIMESTAMP, 
+	last_author VARCHAR(255), 
+	directory BOOL DEFAULT 0, -- is the file a directory?
+	size BIGINT UNSIGNED NOT NULL,
+	checksum CHAR(32) NOT NULL,
+	deleted BOOL DEFAULT 0,
+	FOREIGN KEY (file_Id) REFERENCES files(file_id),
+	PRIMARY KEY (file_id, version) ) ENGINE = INNODB;
+
+-- Simple look up table for permissions
+CREATE TABLE permissions (
+	permission_level TINYINT UNSIGNED PRIMARY KEY,
+	description VARCHAR(255) NOT NULL) ENGINE = INNODB;
 	
 -- Create users_files table to resolve the many-to-many relation between files and users
 CREATE TABLE users_files(
 	username VARCHAR(255) NOT NULL, 
 	file_id BIGINT UNSIGNED NOT NULL,
+	permission_level TINYINT UNSIGNED NOT NULL,
 	FOREIGN KEY (username) REFERENCES users(username),
 	FOREIGN KEY (file_id) REFERENCES files(file_id),
+	FOREIGN KEY (permission_level) REFERENCES permissions(permission_level),
 	PRIMARY KEY (username, file_id)) ENGINE = INNODB;
 
-------------------------------------
+-- =======================================
 -- Patches
-------------------------------------
+-- =======================================
 
 -- patch 1 - date: 2011-3-16 22:27--
 START TRANSACTION;
@@ -61,4 +85,44 @@ ALTER TABLE users_files ADD FOREIGN KEY (file_id) REFERENCES files(file_id);
 ALTER TABLE users DROP COLUMN passwordHash;
 ALTER TABLE users ADD COLUMN password_hash CHAR(64) NOT NULL;
 COMMIT;
--- END Patch 1 ---------------------------------------  
+-- END Patch 1 ---------------------------------------
+
+-- Patch 2 - date: 2011-3-20 12:29 --
+-- Simple look up table for permissions
+CREATE TABLE permissions (
+	permission_level TINYINT UNSIGNED PRIMARY KEY,
+	description VARCHAR(255) NOT NULL) ENGINE = INNODB;
+
+-- link to permissions table	
+ALTER TABLE users_files ADD COLUMN permission_level TINYINT UNSIGNED NOT NULL;
+ALTER TABLE users_files ADD FOREIGN KEY (permission_level) REFERENCES permissions(permission_level);
+
+-- file history and version control
+CREATE TABLE file_history (
+	file_id BIGINT UNSIGNED, 
+	version SMALLINT UNSIGNED,
+	client_path VARCHAR(4096) NOT NULL, 
+	server_path VARCHAR(4096) NOT NULL, 
+	last_modified TIMESTAMP, 
+	last_author VARCHAR(255), 
+	directory BOOL DEFAULT 0, -- is the file a directory?
+	size BIGINT UNSIGNED NOT NULL,
+	checksum CHAR(32) NOT NULL,
+	deleted BOOL DEFAULT 0,
+	FOREIGN KEY (file_Id) REFERENCES files(file_id),
+	PRIMARY KEY (file_id, version) ) ENGINE = INNODB;
+	
+-- modify files table
+ALTER TABLE files DROP FOREIGN KEY files_ibfk_1;
+ALTER TABLE files DROP COLUMN parent;
+ALTER TABLE files DROP COLUMN filename;
+ALTER TABLE files DROP COLUMN path;
+
+ALTER TABLE files ADD COLUMN client_path VARCHAR(4096) NOT NULL;
+ALTER TABLE files ADD COLUMN server_path VARCHAR(4096) NOT NULL;
+ALTER TABLE files ADD COLUMN deleted BOOL DEFAULT 0;
+ALTER TABLE files ADD COLUMN checksum CHAR(32) NOT NULL;
+ALTER TABLE files MODIFY directory BOOL DEFAULT 0;
+
+ALTER TABLE users ADD COLUMN salt CHAR(32) NOT NULL;
+-- END Patch 2 ----------------------------------------
