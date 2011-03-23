0e2907dd522fa4b54d6ebfb323e2ae4dba34b672
mobileProgrammer <Mike@140.193.239.101>
2011-03-22 20:19:15 -0600

    Useful SQL queries for development purposes only

---
+++ Server/_dev.sql
@@ -0,0 +1,7 @@
+-- This file contains some useful SQL for use while doing development
+
+-- clean all data in the database
+TRUNCATE TABLE users_files;
+TRUNCATE TABLE file_history;
+TRUNCATE TABLE files;
+TRUNCATE TABLE users;
